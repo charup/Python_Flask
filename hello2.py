c0fedb8d0c5247680c91d848715ae475cdc2aa6f
Pande <pandec@uta.edu>
2016-05-27 16:34:05 -0500

    Commit different hello programs

--- hello2.py
+++ hello2.py
@@ -2,5 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')				#http://127.0.0.1:5000/
+def index():
+	return 'Index Page'
+
+@app.route('/hello')		#http://127.0.0.1:5000/hello
 def hello():
 	return 'Hello World'
+	
+if __name__ =='__main__':
+	app.run()
